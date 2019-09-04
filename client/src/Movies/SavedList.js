f09e8384dbc59f309a2dcf2e958cd2b8f3ae8ed1
raythurman2386 <raymondthurman5@gmail.com>
2019-09-03 22:58:47 -0400

    final stretch complete

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -5,7 +5,7 @@ const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <Link to={`/movies/${movie.id}`} className="saved-movie">{movie.title}</Link>
     ))}
     <Link to="/"><div className="home-button">Home</div></Link>
   </div>
